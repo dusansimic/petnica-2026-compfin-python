351de67fe12d7d5f77d20b0e6fe3ea001326a071
Dušan Simić <dusan.simic1810@gmail.com>
2026-08-03 15:43:05 +0200

    docs(01_intro): add enumerate examples to for loop tutorial

--- 01_intro/05_for_list.py
+++ 01_intro/05_for_list.py
@@ -7,3 +7,11 @@ for i in range(len(lista)):
 # Iteriranje po elementima
 for element in lista:
     print("Element:", element)
+
+# Iteriranje po elementima sa indeksom
+for i, element in enumerate(lista):
+    print("Element na indeksu", i, "je", element)
+
+# Numeracija koja počinje od 1
+for redni_broj, element in enumerate(lista, start=1):
+    print("Element broj", redni_broj, "je", element)
